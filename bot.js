d30397a0ad57f37b5abcca4b0dca1e98bdbd43fb
flghslkdgsdg <43896154+flghslkdgsdg@users.noreply.github.com>
2019-03-16 22:43:58 +0200

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,35 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+var prefix = "1";
+
+client.on('ready', () => {
+     client.user.setActivity("Alveen ♡.",{type: 'WATCHING'})
+
+});
+
+client.on('ready', async() => {
+var server = "463532329084780545"; // ايدي السررفر
+var channel = "464066001403772929";//ايدي الروم
+    setInterval(()=>{
+    client.guilds.get(server).channels.get(channel).send('**Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , **')
+    },305);
+})
+
+client.on('message', message => {
+  if (message.author.bot) return;
+  if (!message.content.startsWith(prefix)) return;
+
+  let command = message.content.split(" ")[0];
+  command = command.slice(prefix.length);
+
+  let args = message.content.split(" ").slice(1);
+
+  if (command == "say") {
+if(!message.member.hasPermission('ADMINISTRATOR')) return message.channel.send('?|**\`ADMINISTRATOR\`ليس لديك صلاحيات`**');
+   message.channel.sendMessage(args.join("  "))
+   message.delete()
+  }
+ });
+
+ 
+client.login(process.env.BOT_TOKEN);
